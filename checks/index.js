b94d4a280555f6776a0ee0299dfea5e91f04f327
Johnson Samuel <jcharlessamuel@apple.com>
2019-11-27 13:34:53 +0530

    removed checks folder.

--- checks/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    createUnitTestsAnnotations: require('./unit-test-checks'),
-    createOutdatedCheckAnnotations: require('./outdated-checks'),
-    createAuditCheckAnnotations: require('./audit-checks'),
-    createESLintAnnotations: require('./eslint-checks')
-}
